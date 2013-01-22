fac8a45f7ee9bce92924e7a2f0128b0e98f11f20
pkruk <piotr@kruk.co>
2013-01-22 11:56:39 +0100

    Dwuspojne zadanie wykonane

--- dwuspojne_skladowe/Dwuspojne/DFS.java
+++ dwuspojne_skladowe/Dwuspojne/DFS.java
@@ -1,11 +1,23 @@
 import java.util.*;
 
 public class DFS {
+	private int l = 0;
+	public String poczynaj(){
 
-	public void poczynaj(){
-		for (int i = 0; i < g.len; i++)
-			if (visited[i] == false)
+		spojne.add(new ArrayList<Integer>());
+		for (int i = 0; i < g.len; i++){
+			if (visited[i] == false){
+				spojne.get(l).add(i);
+				if (g.sasiedzi.get(i).size() > 1){
+					pktArtykulacji.add(i+1);
+				}
 				dfs(i);
+				++l;
+				spojne.add(new ArrayList<Integer>());
+			}
+		}
+		System.out.println();
+		return wynik();
 	}
 
 	public void dfs(int v){
@@ -18,29 +30,29 @@ public class DFS {
 			if (visited[u] == false){
 				p[u] = v; // poprzednik :D 
 				S.push(tmp);
+				spojne.get(l).add(u); // nieodwiedzone
 				dfs(u);
-				if (low[u] >= nr[v] && (g.sasiedzi.get(v).size() == 1 || g.sasiedzi.get(v).size() > 2))
-					System.out.println("~" + (v));
+				if (low[u] >= nr[v] &&  !IN(pktArtykulacji,v+1)){
+					// System.out.println("~" + (v)); // pkt artykulacji 
+					pktArtykulacji.add(v+1);
+				}
 				if (low[u] >= nr[v]){
 					//Wyznaczamy Dwuspojne skladowe wzdlug punktow artykulacji 
 					int waga = 0;
 					dwuspojne.add(new ArrayList<Integer>());
-
 					do { 
 						tmp = S.peek();
 						S.pop();
 						waga++;
 						//System.out.print((tmp.from +1)+ " -- " + (tmp.to+1) + " ");
 						g.V.get(tmp.from).ojciec = tmp.to +1;
-						dwuspojne.get(x).add(tmp.from +1);
+						if(!IN(dwuspojne.get(x),tmp.from+1))
+							dwuspojne.get(x).add(tmp.from +1);
 					} while(tmp.from != v || tmp.to != u);
-					if (waga == 1)
-						System.out.println(dwuspojne.get(x).get(0) + " " + g.V.get(dwuspojne.get(x).get(0)-1).ojciec +"<-- MOST ");
-					else {
-						for (int j = 0; j < dwuspojne.get(x).size(); j++){
-							System.out.print(dwuspojne.get(x).get(j) + " ");
-						}
-						System.out.println();
+					if (waga == 1){
+						//System.out.println(dwuspojne.get(x).get(0) + " " + g.V.get(dwuspojne.get(x).get(0)-1).ojciec +"<-- MOST ");
+						mosty.add(dwuspojne.get(x).get(0));
+						mosty.add(g.V.get(dwuspojne.get(x).get(0)-1).ojciec);
 					}
 					x++;
 
@@ -61,6 +73,46 @@ public class DFS {
 		p = new int[g.len];
 		for(int i = 0; i < g.len; nr[i] = -1,visited[i] = false, low[i] = -1, ++i);
 	}
+	public String wynik(){
+		System.out.println("===============================================");
+		String wyjscie = "";
+		for(int i = 0; i < spojne.size(); i++){
+			Collections.sort(spojne.get(i));
+			for (int j = 0; j < spojne.get(i).size(); j++)
+				wyjscie += (spojne.get(i).get(j) +1 ) + " ";
+			wyjscie += "\n";
+		}
+		wyjscie += "\n";
+		for (int i = 0; i < dwuspojne.size(); i++){
+			if (dwuspojne.get(i).size() > 2){
+				for (int j = dwuspojne.get(i).size() -1 ; j >= 0; j--){
+					wyjscie += dwuspojne.get(i).get(j) + " ";
+				}
+				wyjscie += "\n";
+			}
+		}
+		wyjscie += "\n";
+		for (int i = 0; i < mosty.size(); i++){
+			if (i % 2 == 0 && i != 0)
+				wyjscie += "\n";
+			wyjscie += mosty.get(i) + " ";
+		}
+		wyjscie += "\n";
+		Collections.sort(pktArtykulacji);
+		for (int i = 0; i < pktArtykulacji.size(); i++)
+			wyjscie += pktArtykulacji.get(i) + " ";
+		//System.out.println(wyjscie);
+		return wyjscie;
+	}
+	public boolean IN(ArrayList<Integer> x , int k){
+		for (int i = x.size() -1; i >= 0; i--)
+			if ( x.get(i) == k)
+				return true;
+		return false;
+	}
+	private ArrayList<ArrayList<Integer>> spojne = new ArrayList<ArrayList<Integer>>();
+	private ArrayList <Integer> pktArtykulacji = new ArrayList<Integer>();
+	private ArrayList<Integer> mosty = new ArrayList<Integer>();
 	private int x = 0;
 	private int time = 0;
 	private boolean visited[];
--- dwuspojne_skladowe/Dwuspojne/Main.java
+++ dwuspojne_skladowe/Dwuspojne/Main.java
@@ -1,17 +1,16 @@
 import java.util.*;
-
+import java.io.*;
 public class Main {
 
-	public static void main(String[] args) {
-		praca();
+	public static void main(String[] args) throws IOException{
+		//praca();
+		txt();
 	}
 
-
-	public static void praca(){
-		Graf g = konsola();
+	public static String praca(Graf g){
 		g.printSasiedzi();
 		DFS t = new DFS(g);
-		t.poczynaj();
+		return t.poczynaj();
 	}
 	public static Graf konsola(){
 			System.out.println("KONSOLA : ");
@@ -28,6 +27,81 @@ public class Main {
 				}
 			}
 			System.out.println(".................................................................");
+		praca(g);
 		return g;
+	}
+
+
+	public static void txt() throws IOException{
+		String a1 = "../testy/input0.txt";
+		String a2 = "../testy/input1.txt";
+		String a3 = "../testy/input2.txt";
+		String a4 = "../testy/input3.txt";
+		String a5 = "../testy/input4.txt";
+		String a6 = "../testy/input5.txt";
+		String a7 = "../testy/input6.txt";
+		String a8 = "../testy/input7.txt";
+		String a9 = "../testy/input8.txt";
+		String a10 = "../testy/input9.txt";
+		_txt(a1);
+		_txt(a2);
+	//	_txt(a3);
+		_txt(a4);
+		_txt(a5);
+		_txt(a6);
+		_txt(a7);
+		_txt(a8);
+		_txt(a9);
+		_txt(a10);
+	}
+
+	public static void _txt(String txt)throws IOException {
+		Scanner plikWej = null;
+		Graf g = new Graf();
+		try {
+			plikWej = new Scanner(new BufferedReader(new FileReader(txt)));
+			int licznik = 0;
+			int rozmiar_grafu = plikWej.nextInt();
+			for(int i = 0; i < rozmiar_grafu; i++)
+				g.add(i);
+			while(plikWej.hasNext()){
+				if(plikWej.hasNextInt()){
+					int rozmiar = plikWej.nextInt();
+					System.out.println("---" + rozmiar);
+					for (int i = 0; i < rozmiar; i++){
+						g.sasiedzi.get(licznik).add(plikWej.nextInt() -1);
+						System.out.print(g.sasiedzi.get(licznik).get(i) + " ");
+					}	
+					System.out.println();
+					licznik++;
+				} else if (plikWej.hasNext())
+					plikWej.next();
+			}
+		} catch (IOException ex){
+			System.err.println("Blad wejscia");
+		} finally {
+			if (plikWej != null)
+				plikWej.close();
+		}
+		//g.printSasiedzi();
+		String wynik = praca(g);
+		FileWriter zapis = null;
+		try { 
+			String gdzie = "../testy/MOJE/";
+			gdzie += "output" + txt.substring(txt.length()-5,txt.length());
+			System.out.println("~" + wynik);
+			zapis = new FileWriter(gdzie);
+			zapis.write(wynik);
+		} catch (IOException ex) {
+			System.err.println("Blad wyjscia");
+		} finally {
+			if (zapis != null)
+				zapis.close();
+		}
+	}
+
+
+	public static void wyswietl(String a){
+		System.out.println("~ " + a);
 	}
 }
